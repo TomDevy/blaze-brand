3251b438666c05622d1081a5fbba31984deab6d5
Emmanuel Agboola <101583530+TomDevy@users.noreply.github.com>
2023-02-13 15:51:23 +0100

    fixing validation

--- src/Contact/contact.jsx
+++ src/Contact/contact.jsx
@@ -9,8 +9,7 @@ import tw from "../../src/components/icons/Frame-2.png";
 import ln from "../../src/components/icons/Frame.png";
 import wh from "../../src/components/icons/Whatsapp Icon.png";
 
-
-import emailjs from '@emailjs/browser'
+import emailjs from "@emailjs/browser";
 
 import "./contact.css";
 const Result = () => {
@@ -40,9 +39,27 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-      if (!firstName || !lastName || !email || !phone || !studyLocation || !studyTime || !studyLevel ) {
-        return alert("Please fill in your details!");
-      }
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !phone ||
+      !studyLocation ||
+      !studyTime ||
+      !studyLevel
+    )
+    {
+      return alert("Please fill in your details!");
+    }
+    console.log(
+      firstName,
+      lastName,
+      email,
+      phone,
+      studyLocation,
+      studyTime,
+      studyLevel
+    );
 
     emailjs.sendForm(serviceId, templateId, form.current, publicKey).then(
       (result) => {
@@ -79,8 +96,8 @@ const Contact = () => {
         <div className="add">
           <h3>CONTACT INFORMATION</h3>
           <p>
-            info@blazebrandlimited.com, helpdesk@blazebrandlimited.com +234 705 327
-            6426, +234 816 305 9266
+            info@blazebrandlimited.com, helpdesk@blazebrandlimited.com +234 705
+            327 6426, +234 816 305 9266
           </p>
         </div>
         <div className="add">
@@ -89,16 +106,28 @@ const Contact = () => {
         </div>
       </div>
       <div className="soc-m">
-        <a target="_blank" href="https://www.instagram.com/invites/contact/?i=esabljrtzhew&utm_content=pg1ufhi">
+        <a
+          target="_blank"
+          href="https://www.instagram.com/invites/contact/?i=esabljrtzhew&utm_content=pg1ufhi"
+        >
           <img src={ig} alt="instagram" />
         </a>
-        <a target="_blank" href="https://www.facebook.com/profile.php?id=100089098602386">
+        <a
+          target="_blank"
+          href="https://www.facebook.com/profile.php?id=100089098602386"
+        >
           <img src={fb} alt="facebook" />
         </a>
-        <a target="_blank" href="https://twitter.com/blazebrandltd1?t=EMKPOsOdx7uVpammVKl5_w&s=09">
+        <a
+          target="_blank"
+          href="https://twitter.com/blazebrandltd1?t=EMKPOsOdx7uVpammVKl5_w&s=09"
+        >
           <img src={tw} alt="twitter" />
         </a>
-        <a target="_blank" href="https://www.linkedin.com/company/blaze-brand-limited/">
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/company/blaze-brand-limited/"
+        >
           <img src={ln} alt="linkedin" />
         </a>
         <a target="_blank" href="https://wa.me/message/T6NMQMJDVUBSH1">
@@ -109,70 +138,102 @@ const Contact = () => {
         <h2>BOOK A FREE APPOINTMENT</h2>
         <p>Provide your details below & we'll get back to you.</p>
         <form ref={form} onSubmit={sendEmail}>
-        <div className="frm-d">
-          <input type="text" name="firstName" placeholder="First name" />
-          <input type="text" name="lastName" placeholder="Last name" />
-          <input type="text" name="email" placeholder="Email address" />
-          <input type="text" name="phone" placeholder="Phone number" />
+          <div className="frm-d">
+            <input
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              type="text"
+              name="firstName"
+              placeholder="First name"
+            />
+            <input
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              type="text"
+              name="lastName"
+              placeholder="Last name"
+            />
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              name="email"
+              placeholder="Email address"
+            />
+            <input
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              type="text"
+              name="phone"
+              placeholder="Phone number"
+            />
             <Select
               name="studyLocation"
-            placeholder="Your Preffered study location"
-            closeMenuOnSelect={true}
-            components={animatedComponents}
-            options={[
-              { value: "United Kingdom", label: "United Kingdom" },
-              { value: "Canada", label: "Canada" },
-              { value: "United States", label: "United States" },
-            ]}
-          />
+              placeholder="Your Preffered study location"
+              closeMenuOnSelect={true}
+              components={animatedComponents}
+              options={[
+                { value: "United Kingdom", label: "United Kingdom" },
+                { value: "Canada", label: "Canada" },
+                { value: "United States", label: "United States" },
+              ]}
+              onChange={(selectedOption) =>
+                setStudyLocation(selectedOption.value)
+              }
+            />
             <Select
               name="studyTime"
-            placeholder="When do you plan study?"
-            closeMenuOnSelect={true}
-            components={animatedComponents}
-            options={[
-              {
-                value: "January",
-                label: `January ${new Date().getFullYear()}`,
-              },
-              { value: "May", label: `May ${new Date().getFullYear()}` },
-              {
-                value: "September",
-                label: `September ${new Date().getFullYear()}`,
-              },
-            ]}
-          />
+              placeholder="When do you plan study?"
+              closeMenuOnSelect={true}
+              components={animatedComponents}
+              options={[
+                {
+                  value: `January ${new Date().getFullYear()}`,
+                  label: `January ${new Date().getFullYear()}`,
+                },
+                {
+                  value: `May ${new Date().getFullYear()}`,
+                  label: `May ${new Date().getFullYear()}`,
+                },
+                {
+                  value: `September ${new Date().getFullYear()}`,
+                  label: `September ${new Date().getFullYear()}`,
+                },
+              ]}
+              onChange={(selectedOption) => setStudyTime(selectedOption.value)}
+            />
             <Select
               name="studyLevel"
-            className="custom-class"
-            placeholder="Preffered study level"
-            closeMenuOnSelect={true}
-            components={animatedComponents}
-            options={[
-              { value: "Doctorate", label: "Doctorate" },
-              { value: "Postgraduate", label: "Postgraduate" },
-              { value: "Undergraduate", label: "Undergraduate" },
-              { value: "University", label: "University" },
-              { value: "Preparation", label: "Preparation" },
-              { value: "Vocational", label: "Vocational" },
-              { value: "English Language", label: "English Language" },
-            ]}
-          />
-          <label htmlFor="">
-            <input className="checkb" type="checkbox" />
-            Subscribe to our Newsletter
-          </label>
-          <button
-            type="submit"
-            style={{
-              backgroundColor: isSubmitted ? "green" : "#B50000",
-            }}
-          >
-            {isSubmitted ? "Booked" : "Book Appointment"}
-          </button>
-          <div className="form-result">{result ? <Result /> : null}</div>
+              className="custom-class"
+              placeholder="Preffered study level"
+              closeMenuOnSelect={true}
+              components={animatedComponents}
+              options={[
+                { value: "Doctorate", label: "Doctorate" },
+                { value: "Postgraduate", label: "Postgraduate" },
+                { value: "Undergraduate", label: "Undergraduate" },
+                { value: "University", label: "University" },
+                { value: "Preparation", label: "Preparation" },
+                { value: "Vocational", label: "Vocational" },
+                { value: "English Language", label: "English Language" },
+              ]}
+              onChange={(selectedOption) => setStudyLevel(selectedOption.value)}
+            />
+            <label htmlFor="">
+              <input className="checkb" type="checkbox" />
+              Subscribe to our Newsletter
+            </label>
+            <button
+              type="submit"
+              style={{
+                backgroundColor: isSubmitted ? "green" : "#B50000",
+              }}
+            >
+              {isSubmitted ? "Booked" : "Book Appointment"}
+            </button>
+            <div className="form-result">{result ? <Result /> : null}</div>
           </div>
-          </form>
+        </form>
       </div>
     </div>
   );
